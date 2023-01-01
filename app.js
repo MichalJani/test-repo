2007771e1d27e52dc24fc0db25f95ab3da2873ba
Michal <michal@janicki.dev>
2023-01-01 21:22:35 +0100

    update
    Signed-off-by: Michal <michal@janicki.dev>

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-
-  res.send('Hello World!');
-});
-
 
 app.get('/bro', (req, res) => {
   res.set('X-bro', 'BRO');
   res.status(418);
   res.send('MATE!');
 });
+app.get('/', (req, res) => {
+
+  res.send('Hello Universe!');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
